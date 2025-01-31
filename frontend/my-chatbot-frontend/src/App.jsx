36b2ca4a1e76b4fb1d560edd1ce98efb7e5d65af
théophile <theophilebaudry02@gmail.com>
2025-01-31 02:15:54 -0500

    Initial version of the Frontend using React

---
+++ frontend/my-chatbot-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import ChatContainer from './components/Chat/ChatContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <ChatContainer />
+    </div>
+  );
+}
+
+export default App;
